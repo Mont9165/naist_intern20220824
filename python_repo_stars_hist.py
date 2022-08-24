8b4c057aa115e338690082527f95cf878a6eed46
horikawa <kosei9165@gmail.com>
2022-08-24 16:10:16 +0900

    add py star histgram

--- python_repo_stars_hist.py
+++ python_repo_stars_hist.py
@@ -1,6 +1,6 @@
 from github import Github
-import matplotlib.pyplot as plt
 import seaborn
+import matplotlib.pyplot as plt
 
 # First create a Github instance:
 token = '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'
@@ -11,21 +11,16 @@ g = Github(token)
 #g = Github(base_url="https://Mont9165/api/v3", login_or_token="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")
 
 # Then play with your Github objects:
-repositories = g.search_repositories(query='stars:>10000', sort='stars')
+repositories = g.search_repositories(query='language:python, stars:>5000', sort='stars')
 count = 0
-repo_star_num = []
+repo_star = []
 for repo in repositories:
     if count < 500:
-        repo_star_num.append(repo.stargazers_count)
+        repo_star.append(repo.stargazers_count)
     else:
         break
     count += 1
-    
-plt.hist(repo_star_num)
-plt.title('star_number')
-plt.xlabel('star_num')
-plt.ylabel('repo_num')
-#plt.show()
-seaborn.histplot(repo_star_num)
 
-#plt.savefig('repo_star_histgram.png')
+seaborn.histplot(repo_star)
+plt.show()
+plt.savefig("py_repo_star_histgram.png")
