4e983b1fce18b8666c409107982b78abf580fc2d
horikawa <kosei9165@gmail.com>
2022-08-24 14:22:41 +0900

    add python star rank

---
+++ python_repo_stars_ranking.py
@@ -0,0 +1,19 @@
+from github import Github
+
+# First create a Github instance:
+token = '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'
+# using an access token
+g = Github(token)
+
+# Github Enterprise with custom hostname
+#g = Github(base_url="https://Mont9165/api/v3", login_or_token="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")
+
+# Then play with your Github objects:
+repositories = g.search_repositories(query='language:python, stars:>100000', sort='stars')
+count = 0
+for repo in repositories:
+    if count < 5:
+        print(repo.html_url)
+    else:
+        break
+    count += 1
