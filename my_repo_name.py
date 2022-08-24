766b9504e4024eb445ef4602017ee9f3a8a3d7b8
horikawa <kosei9165@gmail.com>
2022-08-24 13:55:00 +0900

    pyGithub test

---
+++ my_repo_name.py
@@ -0,0 +1,14 @@
+from github import Github
+
+# First create a Github instance:
+token = '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'
+# using an access token
+g = Github(token)
+
+# Github Enterprise with custom hostname
+#g = Github(base_url="https://Mont9165/api/v3", login_or_token="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")
+
+# Then play with your Github objects:
+for repo in g.get_user().get_repos(type='owner'):
+    print(repo.name)
+
